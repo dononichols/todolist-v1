536f8f6e427d651973bf548663f6b97fa2a296c8
Meraki_DNA <dononichols@gmail.com>
2022-07-20 17:01:15 -0400

    -Template: server setup (w/notes)

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 //jshint esversion:6
 
+//////////////
+// requiring our two packages we just installed: express and body-parser
 const express = require("express");
 const bodyParser = require("body-parser");
 
+
+////////////
+// creates our app const by using express.
 const app = express();
 
+
+//////////
+// create a simple get route, that sends the browser the word, "Hello", when a user trys to access the Home route "/."
 app.get("/", function(req, res){
     res.send("Hello");
 });
 
 
 
-
+///////
+// Listen on port 3000 that our server has been started.
 app.listen(3000, function(){
     console.log("Server started on Port 3000 !")
 });
